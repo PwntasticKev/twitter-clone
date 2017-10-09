bf5dcc983a43774495e2c295393b410440b41d3d
Kevin Lee <pwntastickevin@gmail.com>
2017-10-09 16:52:53 -0600

    tweeeet

--- js/animations.js
+++ js/animations.js
@@ -2,32 +2,27 @@ $(document).ready(function(){
   $('#char-count').hide();
   $('#tweet-submit').hide();
 
-$('.tweet-compose').on('click', function(){
-  $('#char-count').show();
-  $('#tweet-submit').show();
-  $(this).css("height", "5rem")
-});
+  $('.tweet-compose').on('click', function(){
+    $('#tweet-submit').show();
+    $('#char-count').show();
+    $(this).css('height', '5rem')
+  });
 
-var textMax = 140;
-$('.tweet-compose').on('keyup', function() {
-$('#char-count').html(140 - $(this).val().length) ;
+  var textMax = 140;
+  $('.tweet-compose').on('keyup', function() {
+    var charLeft = $('#tweet-compose').val().length
+    $('#char-count').html( textMax - charLeft)
+      if (charLeft < 0) {
+        return charLeft = $('.tweet-compose').val('').length
+      }
 
+  })
 
+  $('#tweet-submit').on('click', function() {
+    var tweet = $('#compose').prepend()
+    $('#stream').prepend(tweet)
 
-
-  // $('#char-count').val($('#char-count').val().length <= 140)
-
-
-  // console.log($('#char-count').text())
-//   1. get the text value of an input
-// 2. get the length of that value
-// 3. set the html of an element based on that value
-})
-
-$('#tweet-submit').on('click', function() {
-  $('.tweet-compose').prependTo('.content'));
-  $('#profile-summary').prependTo('.tweet');
-})
+  });
 
 
 });
